683857923e19a98d9809b170e935156ebd18bd69
OlegShyian <92789608+OlegShyian@users.noreply.github.com>
2022-09-12 15:08:38 +0000

    Deploying to gh-pages from @ OlegShyian/goit-js-hw-09@2c19c379fb1915bae36903bf066bb4fc5b14e8f7 🚀

---
+++ 01-color-switcher.703187d3.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");let o=null;function d(){const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;n.style.backgroundColor=t}t.addEventListener("click",(function(){d(),o=setInterval((()=>{d()}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.703187d3.js.map
